ce431f1be3132ee8f4dd777dc1bfd9a550b01417
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-04-26 17:11:28 -0300

    iniciando o projeto account-control

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises
+const path = require('path')
+const route = path.resolve(__dirname, './users.json')
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,6 +10,11 @@ app.use(bodyParser.json());
 app.get('/', (_request, response) => {
   response.status(200).send();
 });
+
+app.get('/users', async (_request, response)=> {
+  const users = await fs.readFile(route, 'utf-8')
+  response.status(200).send(JSON.parse(users))
+})
 
 app.listen('3001', () => {
   console.log('Online');
