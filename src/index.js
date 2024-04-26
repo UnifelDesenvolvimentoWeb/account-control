9eb48e3f51c8a5a798e15891d8b1d65976b8d60f
GabrieiBro <gabriel.fsans@hotmail.com>
2024-04-26 16:37:22 -0300

    get(/users)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
+const CWD = process.env.PWD;
 app.use(bodyParser.json());
 
 app.get('/', (_request, response) => {
@@ -11,3 +14,12 @@ app.get('/', (_request, response) => {
 app.listen('3001', () => {
   console.log('Online');
 });
+
+app.get('/users', async (_req, res,) => {
+
+	let usersPath = path.join(CWD, '/src/users.json')
+	let users = await fs.readFile(usersPath, 'UTF-8');
+	users = JSON.parse(users)
+	res.status(200).json(users)
+})
+
