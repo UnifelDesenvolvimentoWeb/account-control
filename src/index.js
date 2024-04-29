7fd49430bd024e92a9850c914b64463b5966bad2
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-04-29 16:04:27 -0300

    Requisito 03 completo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const fs = require('fs').promises
 const path = require('path');
 const { request } = require('http');
 const route = path.resolve(__dirname, './users.json')
+const {validateEmail, validatePassword} = require('./middlewares')
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,14 +19,26 @@ app.get('/users', async (_request, response)=> {
 })
 
 app.get('/users/:id', async (request, response)=> {
-  const users = await fs.readFile(route, 'utf-8')
-  const dataUsers = JSON.parse(users)
+  let users = await fs.readFile(route, 'utf-8')
+  users = JSON.parse(users)
   const id = request.params.id
-  const userFound = dataUsers.find((user)=> user.id === Number(id))
+  const userFound = users.find((user)=> user.id === Number(id))
   if (userFound) {
    return response.status(200).json(userFound)
   }
   return response.status(404).json({"message": "Usuário não encontrado"})
+})
+
+app.post('/login', validateEmail, validatePassword, async (request, response)=>{
+  const { email, password } = request.body
+  let users = await fs.readFile(route, 'utf-8')
+  users = JSON.parse(users)
+  let user = users.find((user)=> user.email === email && user.password === password)
+
+  if (!user) {
+    return response.status(401).json({"message": "Email ou senha incorretos"})
+  }
+  response.status(200).json({"message": "Login realizado com sucesso"})
 })
 
 app.listen('3001', () => {
