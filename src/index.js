093d13c8bcf23f948d3051a4994f9d6687b16fc7
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-04-29 14:15:57 -0300

    Requisito 02 completo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises
-const path = require('path')
+const path = require('path');
+const { request } = require('http');
 const route = path.resolve(__dirname, './users.json')
 
 const app = express();
@@ -14,6 +15,17 @@ app.get('/', (_request, response) => {
 app.get('/users', async (_request, response)=> {
   const users = await fs.readFile(route, 'utf-8')
   response.status(200).send(JSON.parse(users))
+})
+
+app.get('/users/:id', async (request, response)=> {
+  const users = await fs.readFile(route, 'utf-8')
+  const dataUsers = JSON.parse(users)
+  const id = request.params.id
+  const userFound = dataUsers.find((user)=> user.id === Number(id))
+  if (userFound) {
+   return response.status(200).json(userFound)
+  }
+  return response.status(404).json({"message": "Usuário não encontrado"})
 })
 
 app.listen('3001', () => {
