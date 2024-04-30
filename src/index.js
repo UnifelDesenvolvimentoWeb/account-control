7deec4b4fcef53cb775d3d0d43a3b5cada7581b2
Helena Estrela <estrelahelena7@gmail.com>
2024-04-30 13:08:19 -0300

    GET /users/:id - feito

--- src/index.js
+++ src/index.js
@@ -11,10 +11,26 @@ app.get('/', (_request, response) => {
   response.status(200).send();
 });
 
+//requisito 01
 app.get('/users', async (_req, res) => {
   const users = await fs.readFile(route, 'UTF-8')
 
   return res.status(200).json(JSON.parse(users))
 });
+
+//requisito 02
+app.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const users = await fs.readFile(route, 'UTF-8');
+  const updUsers = JSON.parse(users)
+
+  const user = updUsers.find((user) => user.id === +id);
+  if (!user) {
+    return res.status(404).json({message: 'Usuário não encontrado'})
+  }
+
+  return res.status(200).json(user)
+})
 
 module.exports = app;
