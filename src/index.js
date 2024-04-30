9e583694b00e7c716d1035c5e1bb6f1fe2e76ff2
Helena Estrela <estrelahelena7@gmail.com>
2024-04-30 11:53:20 -0300

    GET /users - feito

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require('fs').promises
+const path = require('path');
+const bodyParser = require ('body-parser');
 
 const app = express();
+const route = path.resolve('./src/users.json');
 app.use(bodyParser.json());
 
 app.get('/', (_request, response) => {
   response.status(200).send();
 });
 
-app.listen('3001', () => {
-  console.log('Online');
+app.get('/users', async (_req, res) => {
+  const users = await fs.readFile(route, 'UTF-8')
+
+  return res.status(200).json(JSON.parse(users))
 });
+
+module.exports = app;
