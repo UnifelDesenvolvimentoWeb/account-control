55bc3ed773fa1684e92a7ffbf3463985781b87a8
Jean Wilker <jeanwilkerss@gmail.com>
2024-05-01 17:39:00 -0300

    Cria rota para obter usuário.

--- src/index.js
+++ src/index.js
@@ -13,10 +13,15 @@ app.get('/', (_request, response) => {
   response.status(200).send();
 });
 
+async function readUsersData() {
+  const usersData = await fs.readFile(route, 'utf8');
+  return JSON.parse(usersData);
+}
+
+// Rota para obter todos os usuários
 app.get('/users', async (_request, response) => {
   try {
-    const usersData = await fs.readFile(route, 'utf8');
-    const users = JSON.parse(usersData);
+    const users = await readUsersData();
 
     if (users.length === 0) {
       response.status(200).json([]);
@@ -27,6 +32,25 @@ app.get('/users', async (_request, response) => {
     response.status(500).json({ error: 'Erro ao processar a requisição' });
   }
 });
+
+// Rota para obter um usuário específico
+app.get('/users/:id', async (request, response) => {
+  try {
+    const users = await readUsersData();
+    const userId = parseInt(request.params.id);
+
+    const user = users.find((user) => user.id === userId);
+
+    if (!user) {
+      response.status(404).json({ message: 'Usuário não encontrado' });
+    } else {
+      response.status(200).json(user);
+    }
+  } catch (error) {
+    response.status(500).json({ error: 'Erro ao processar a requisição' });
+  }
+});
+
 
 app.listen('3001', () => {
   console.log('Online');
