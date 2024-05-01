c09ba35c57f9fa971eba3b42d655ee96436378cc
Jean Wilker <jeanwilkerss@gmail.com>
2024-05-01 18:46:53 -0300

    Cria rota para validar login

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const path = require('path');
+const { validateLogin } = require('./middleware');
 
 const app = express();
 
@@ -51,6 +52,31 @@ app.get('/users/:id', async (request, response) => {
   }
 });
 
+// Rota para validar login
+app.post('/login', validateLogin, async (request, response) => {
+  const { email, password } = request.body;
+
+  try {
+    const users = await readUsersData();
+    const user = users.find(
+      (user) => user.email === email && user.password === password,
+    );
+
+    if (!user) {
+      return response
+        .status(401)
+        .json({ message: 'Email ou senha incorretos' });
+    }
+
+    return response
+      .status(200)
+      .json({ message: 'Login realizado com sucesso' });
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ error: 'Erro ao processar a requisição' });
+  }
+});
 
 app.listen('3001', () => {
   console.log('Online');
--- src/middleware.js
+++ src/middleware.js
@@ -1,1 +1,29 @@
+function validateLogin(req, res, next) {
+  const { email, password } = req.body;
 
+  if (!email || email.trim() === '') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!/\S+@\S+\.\S+/.test(email)) {
+    return res
+      .status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (!password || password.trim() === '') {
+    return res
+      .status(400)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 8) {
+    return res
+      .status(400)
+      .json({ message: 'O "password" deve ter pelo menos 8 caracteres' });
+  }
+
+  next();
+}
+
+module.exports = { validateLogin };
